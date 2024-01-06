4d19f2ac22b70d4196f8a941227b0f180c8751e5
Ranchoo28 <savcreaa.kr99@gmail.com>
2024-01-06 12:38:29 +0100

    Configurazione delle Jwt da finire e riorganizzare classi (da finire).

--- Progetto/Backend/src/main/java/it/unical/demacs/backend/Persistenza/Model/User.java
+++ Progetto/Backend/src/main/java/it/unical/demacs/backend/Persistenza/Model/User.java
@@ -1,7 +1,13 @@
 package it.unical.demacs.backend.Persistenza.Model;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
@@ -10,7 +16,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @EqualsAndHashCode
 
-public class User {
+public class User implements UserDetails {
     private Long idUser;
     private String username;
     private String password;
@@ -18,6 +24,31 @@ public class User {
     private String surname;
     private String email;
     private char role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return Objects.equals(new SimpleGrantedAuthority(String.valueOf(role)), new SimpleGrantedAuthority("u"));
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     //private ArrayList<Booking> bookings=new ArrayList<>();
 
