7ba92b6e66d68daf3bb81b354688015944bb38d2
V3ctor02 <149057899+V3ctor02@users.noreply.github.com>
2024-01-19 14:05:55 +0100

    aggiunti dei mat-grid-list alla galleria(servono altre immagini sennò possiamo lasciare anche queste e sti cà)

--- Progetto/Frontend/src/app/componenti/galleria/galleria.component.ts
+++ Progetto/Frontend/src/app/componenti/galleria/galleria.component.ts
@@ -1,5 +1,10 @@
 import { Component,OnInit } from '@angular/core';
-
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-galleria',
@@ -14,6 +19,24 @@ export class GalleriaComponent{
     { src: 'taglio-donna1.png', title: 'Titolo 2', subtitle: 'Sottotitolo 2' },
     { src: 'taglio-uomo1.png', title: 'Titolo 2', subtitle: 'Sottotitolo 2' },
   ];
+
+  tiles = [
+    {cols: 3, rows: 1,image: './assets/images/Carousel/taglio.png'},
+    {cols: 1, rows: 2,image: './assets/images/Carousel/taglio-donna1.png'},
+    {cols: 1, rows: 1,image: './assets/images/Carousel/taglio-uomo1.png'},
+    {cols: 2, rows: 1,image: './assets/images/Carousel/taglio.png'},
+
+];
+  tiles2 = [
+    {cols: 1, rows: 2,image: './assets/images/Carousel/taglio-uomo1.png'},
+    {cols: 1, rows: 1,image: './assets/images/Carousel/taglio-donna1.png'},
+    {cols: 2, rows: 1,image: './assets/images/Carousel/taglio.png'},
+    {cols: 3, rows: 1,image: './assets/images/Carousel/taglio.png'},
+
+
+  ];
+
+
 
 }
 
